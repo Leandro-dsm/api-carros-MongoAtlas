7e36e795afe381b437c99371e467a7d430a4a41a
Leandro-dsm <leandrosueoka@gmail.com>
2025-09-30 20:15:48 -0300

    Update carroRoutes.js

--- routes/carroRoutes.js
+++ routes/carroRoutes.js
@@ -3,10 +3,19 @@ import carroController from "../controllers/carroController.js";
 
 const carroRoutes = express.Router();
 
+// Listar todos os carros
 carroRoutes.get("/carros", carroController.listarCarros);
+
+// Criar carro
 carroRoutes.post("/carros", carroController.criarCarro);
-carroRoutes.delete("/carros/:id", carroController.deletarCarro);
-carroRoutes.put("/carros/:id", carroController.atualizarCarro);
-carroRoutes.get("/carros/:id", carroController.listarUmCarro);
+
+// Atualizar carro (id no body)
+carroRoutes.put("/carros", carroController.atualizarCarro);
+
+// Deletar carro (id no body)
+carroRoutes.delete("/carros", carroController.deletarCarro);
+
+// Listar 1 carro (id no body)
+carroRoutes.get("/carros/um", carroController.listarUmCarro);
 
 export default carroRoutes;
